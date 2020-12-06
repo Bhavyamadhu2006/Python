57d4895801a55bc56f14cfa4c7b727b811dd2a82
start-python1 <start.python1@gmail.com>
2020-12-06 14:28:51 -0600

    sets and tuples

---
+++ src/Data Types/sets.py
@@ -0,0 +1,15 @@
+my_set = set()
+print(my_set)
+
+my_set.add(1)
+print(my_set)
+
+my_set.add(2)
+print(my_set)
+
+my_set.add(1)
+print(my_set)
+
+my_list = [1,1,1,1,1,1,2,2,2,2,2,2,3,3,3,3,3,3,4,4,4,4,5,5,5,5,5]
+new_set = set(my_list)
+print(new_set)
