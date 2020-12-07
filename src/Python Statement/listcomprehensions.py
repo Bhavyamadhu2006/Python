87bff091890c5ac06e19a2893f79505aad22052c
Bhavyamadhu2006 <50788833+Bhavyamadhu2006@users.noreply.github.com>
2020-12-07 16:42:14 -0600

    Create listcomprehensions.py

---
+++ src/Python Statement/listcomprehensions.py
@@ -0,0 +1,28 @@
+mystring = "hello"
+mylist = []
+
+for letter in mystring:
+  mylist.append(letter)
+
+print(mylist)
+
+# using list comprehensions
+
+mystring2 = "World"
+mylist1 = [letter for letter in mystring2]
+print(mylist1)
+
+# Another example
+
+my_nums = [num for num in range(0,20) if num%5 == 0]
+print(my_nums)
+
+# One more example
+
+value = [x*y for x in [1,2,3] for y in [10,100,1000]]
+print(value)
+
+# Last one
+
+results = ["Fizz - {}".format(num) for num in range(0,10) if num%5 == 0]
+print(results)
